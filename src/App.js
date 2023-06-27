8953f1ef405b811ecbe7d37282223fa5226c08d5
dreinicke27 <danicareinicke@gmail.com>
2023-06-27 16:59:45 -0700

    added form components to app to see what they look like

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
+import NewBoardForm from './components/NewBoardForm';
+import NewCardForm from './components/NewCardForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Inspiration Board</h1>
       </header>
+      <main>
+        <div><NewBoardForm /></div>
+      </main><NewCardForm />
     </div>
   );
 }
