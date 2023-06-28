cc5f3923bf0ab5f11132c23f6cceea99b8bf5b5b
dreinicke27 <danicareinicke@gmail.com>
2023-06-27 19:21:44 -0700

    Added a little css to test how it comes out of Figma

--- src/App.js
+++ src/App.js
@@ -11,9 +11,18 @@ function App() {
         <h1>Inspiration Board</h1>
       </header>
       <main>
-        <div><NewBoardForm /></div>
-      </main><NewCardForm />
-    </div>
+        <div className="Form-list">
+          <h2>Select a Board</h2>
+          <h3>Create a New Board</h3>
+          <div><NewBoardForm /></div>
+          <h3>Create a New Card</h3>
+          <div><NewCardForm /></div>
+        </div>
+        <div className='Board'>
+          <h2>Board Title</h2>
+        </div>
+      </main>
+      </div>
   );
 }
 
