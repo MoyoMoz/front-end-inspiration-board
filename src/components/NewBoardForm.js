0b23655219c4a05f041bfea1c86ac0f78d17dcb4
dreinicke27 <danicareinicke@gmail.com>
2023-07-06 19:08:09 -0700

    Added show/hide new board form functionality

--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import "./NewBoardForm.css";
 
 function NewBoardForm({ createNewBoard }) {
   const [title, setTitle] = useState('');
   const [owner, setOwner] = useState('');
+  const [showForm, setShowForm] = useState(true);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -12,17 +14,30 @@ function NewBoardForm({ createNewBoard }) {
     setOwner('');
   };
 
+
+
   return (
     <form onSubmit={handleSubmit}>
+      <div>
+      <label className="toggle">
+        <input type="checkbox"  onChange={(event) => setShowForm(!showForm)}/>
+        <span className="slider"></span>
+        <span className="labels" data-on="SHOW" data-off="HIDE"></span>
+      </label>
+      </div>
+      <div className={showForm.toString()}>
       <label>
         Title:
         <input type="text" value={title} onChange={(event) => setTitle(event.target.value)} />
       </label>
+      </div>
+      <div className={showForm.toString()}>
       <label>
         Owner:
         <input type="text" value={owner} onChange={(event) => setOwner(event.target.value)} />
       </label>
       <input type="submit" value="Create Board" />
+      </div>
     </form>
   );
 }
