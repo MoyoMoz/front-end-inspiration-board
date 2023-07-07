c125ce4e5d7c5b65d20adfa800ccc04164d4bf1b
dreinicke27 <danicareinicke@gmail.com>
2023-07-06 18:46:21 -0700

    Added input validation to new card form

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -1,26 +1,35 @@
 import React, { useState } from 'react';
+import "./NewCardForm.css";
 
 function NewCardForm( {createCard, boardId} ) {
   const [message, setMessage] = useState('');
+  const [error, setError] = useState('');
+
+  const validateForm = () => {
+    if (message.length > 40 ) {
+      setError("Input cannot be longer than 40 characters");
+    } else if (message.length === 0) {
+      setError("Input cannot be empty");
+    } else {
+      setMessage(message);
+    }
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    createCard(boardId, message);
-    setMessage('');
-
-  //   axios.post('http://127.0.0.1:5000/cards', { message })
-  //     .then((response) => {
-  //       console.log(response);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error creating new card: ", error);
-  //     });
+    validateForm();
+    if (error.length > 1) {
+      createCard(boardId, message);
+      setMessage('');
+      setError('');
+    };
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label>
         Message:
+        <span className="error">{error}</span>
         <input type="text" value={message} onChange={(event) => setMessage(event.target.value)} />
       </label>
       <input type="submit" value="Create Card" />
@@ -29,3 +38,5 @@ function NewCardForm( {createCard, boardId} ) {
 }
 
 export default NewCardForm;
+
+
